506425da5e60da8a01613901f8b305426a487a1c
Arnaud Bernaille <arbernaille2@gmail.com>
2021-10-02 11:57:51 -0500

    express serve statics file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = "localhost";
 const port = "3000";
 
-const app = express();
+const app = express(); // permet de permet de parametrer toutes les requetes. Si je requete http://localhost:3000/fichierQuiExiste => va me le renvoyer, sinon va me renvoyer sur la page d'acceuil.
+app.use(morgan('dev')); // Un nouveau middleware pour afficher des truc dans la console.
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-	console.log(req.headers);
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	res.end('<html><body> Bonjour je suis un server express. Que puis-je faire pour vous ? </body></html>');
